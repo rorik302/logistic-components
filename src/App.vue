df7cc17ed3b1a28d8e4cc6af1712dda20714862d
Rorik <rorik302@gmail.com>
2022-11-05 12:26:04 +0300

    Демо контейнер

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="demo__wrapper"></div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  .demo__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
